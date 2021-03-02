33c8d19818169a801dfe3e68719315e45f909b87
dylan <dylannelson58@gmail.com>
2021-03-02 08:39:05 -0700

    add formatting

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MathContestForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -29,10 +29,30 @@ Partial Class Form1
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.SummaryButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.NameTextBox = New System.Windows.Forms.TextBox()
+        Me.AgeLabel = New System.Windows.Forms.Label()
+        Me.GradeLabel = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.CurrentMathProblemGroupBox.SuspendLayout()
+        Me.StudentInformationGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'CurrentMathProblemGroupBox
         '
+        Me.CurrentMathProblemGroupBox.Controls.Add(Me.TextBox5)
+        Me.CurrentMathProblemGroupBox.Controls.Add(Me.TextBox4)
+        Me.CurrentMathProblemGroupBox.Controls.Add(Me.TextBox1)
+        Me.CurrentMathProblemGroupBox.Controls.Add(Me.Label3)
+        Me.CurrentMathProblemGroupBox.Controls.Add(Me.Label2)
+        Me.CurrentMathProblemGroupBox.Controls.Add(Me.Label1)
         Me.CurrentMathProblemGroupBox.Location = New System.Drawing.Point(12, 133)
         Me.CurrentMathProblemGroupBox.Name = "CurrentMathProblemGroupBox"
         Me.CurrentMathProblemGroupBox.Size = New System.Drawing.Size(200, 239)
@@ -42,6 +62,12 @@ Partial Class Form1
         '
         'StudentInformationGroupBox
         '
+        Me.StudentInformationGroupBox.Controls.Add(Me.TextBox3)
+        Me.StudentInformationGroupBox.Controls.Add(Me.TextBox2)
+        Me.StudentInformationGroupBox.Controls.Add(Me.GradeLabel)
+        Me.StudentInformationGroupBox.Controls.Add(Me.AgeLabel)
+        Me.StudentInformationGroupBox.Controls.Add(Me.NameTextBox)
+        Me.StudentInformationGroupBox.Controls.Add(Me.NameLabel)
         Me.StudentInformationGroupBox.Location = New System.Drawing.Point(12, 12)
         Me.StudentInformationGroupBox.Name = "StudentInformationGroupBox"
         Me.StudentInformationGroupBox.Size = New System.Drawing.Size(406, 115)
@@ -94,11 +120,107 @@ Partial Class Form1
         Me.ExitButton.Text = "Exit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
-        'Form1
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(3, 40)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(45, 17)
+        Me.NameLabel.TabIndex = 0
+        Me.NameLabel.Text = "Name"
+        '
+        'NameTextBox
+        '
+        Me.NameTextBox.Location = New System.Drawing.Point(6, 60)
+        Me.NameTextBox.Name = "NameTextBox"
+        Me.NameTextBox.Size = New System.Drawing.Size(194, 22)
+        Me.NameTextBox.TabIndex = 1
+        '
+        'AgeLabel
+        '
+        Me.AgeLabel.AutoSize = True
+        Me.AgeLabel.Location = New System.Drawing.Point(240, 40)
+        Me.AgeLabel.Name = "AgeLabel"
+        Me.AgeLabel.Size = New System.Drawing.Size(33, 17)
+        Me.AgeLabel.TabIndex = 2
+        Me.AgeLabel.Text = "Age"
+        '
+        'GradeLabel
+        '
+        Me.GradeLabel.AutoSize = True
+        Me.GradeLabel.Location = New System.Drawing.Point(310, 40)
+        Me.GradeLabel.Name = "GradeLabel"
+        Me.GradeLabel.Size = New System.Drawing.Size(48, 17)
+        Me.GradeLabel.TabIndex = 3
+        Me.GradeLabel.Text = "Grade"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(240, 60)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(31, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(316, 60)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(31, 22)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(188, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(7, 125)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(187, 22)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(7, 186)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(193, 22)
+        Me.TextBox5.TabIndex = 5
+        '
+        'MathContestForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(596, 387)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.SummaryButton)
         Me.Controls.Add(Me.ClearButton)
@@ -106,8 +228,12 @@ Partial Class Form1
         Me.Controls.Add(Me.MathProblemTypeGroupBox)
         Me.Controls.Add(Me.StudentInformationGroupBox)
         Me.Controls.Add(Me.CurrentMathProblemGroupBox)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "MathContestForm"
+        Me.Text = "Math Contest"
+        Me.CurrentMathProblemGroupBox.ResumeLayout(False)
+        Me.CurrentMathProblemGroupBox.PerformLayout()
+        Me.StudentInformationGroupBox.ResumeLayout(False)
+        Me.StudentInformationGroupBox.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -119,4 +245,16 @@ Partial Class Form1
     Friend WithEvents ClearButton As Button
     Friend WithEvents SummaryButton As Button
     Friend WithEvents ExitButton As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GradeLabel As Label
+    Friend WithEvents AgeLabel As Label
+    Friend WithEvents NameTextBox As TextBox
+    Friend WithEvents NameLabel As Label
 End Class
--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Math_Contest.Form1
+            Me.MainForm = Global.Math_Contest.MathContestForm
         End Sub
     End Class
 End Namespace
