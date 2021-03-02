d1c8d7425a49c8320bae921c99ca068492a05652
dylan <dylannelson58@gmail.com>
2021-03-02 08:20:14 -0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CurrentMathProblemGroupBox = New System.Windows.Forms.GroupBox()
+        Me.StudentInformationGroupBox = New System.Windows.Forms.GroupBox()
+        Me.MathProblemTypeGroupBox = New System.Windows.Forms.GroupBox()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SummaryButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CurrentMathProblemGroupBox
+        '
+        Me.CurrentMathProblemGroupBox.Location = New System.Drawing.Point(12, 133)
+        Me.CurrentMathProblemGroupBox.Name = "CurrentMathProblemGroupBox"
+        Me.CurrentMathProblemGroupBox.Size = New System.Drawing.Size(200, 239)
+        Me.CurrentMathProblemGroupBox.TabIndex = 0
+        Me.CurrentMathProblemGroupBox.TabStop = False
+        Me.CurrentMathProblemGroupBox.Text = "Current Math Problem"
+        '
+        'StudentInformationGroupBox
+        '
+        Me.StudentInformationGroupBox.Location = New System.Drawing.Point(12, 12)
+        Me.StudentInformationGroupBox.Name = "StudentInformationGroupBox"
+        Me.StudentInformationGroupBox.Size = New System.Drawing.Size(406, 115)
+        Me.StudentInformationGroupBox.TabIndex = 1
+        Me.StudentInformationGroupBox.TabStop = False
+        Me.StudentInformationGroupBox.Text = "Student Information"
+        '
+        'MathProblemTypeGroupBox
+        '
+        Me.MathProblemTypeGroupBox.Location = New System.Drawing.Point(218, 133)
+        Me.MathProblemTypeGroupBox.Name = "MathProblemTypeGroupBox"
+        Me.MathProblemTypeGroupBox.Size = New System.Drawing.Size(200, 239)
+        Me.MathProblemTypeGroupBox.TabIndex = 2
+        Me.MathProblemTypeGroupBox.TabStop = False
+        Me.MathProblemTypeGroupBox.Text = "Math Problem Type"
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.Location = New System.Drawing.Point(424, 32)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(148, 77)
+        Me.SubmitButton.TabIndex = 3
+        Me.SubmitButton.Text = "Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(424, 115)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(148, 77)
+        Me.ClearButton.TabIndex = 4
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SummaryButton
+        '
+        Me.SummaryButton.Location = New System.Drawing.Point(424, 198)
+        Me.SummaryButton.Name = "SummaryButton"
+        Me.SummaryButton.Size = New System.Drawing.Size(148, 77)
+        Me.SummaryButton.TabIndex = 5
+        Me.SummaryButton.Text = "Summary"
+        Me.SummaryButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(424, 281)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(148, 77)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SummaryButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.MathProblemTypeGroupBox)
+        Me.Controls.Add(Me.StudentInformationGroupBox)
+        Me.Controls.Add(Me.CurrentMathProblemGroupBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CurrentMathProblemGroupBox As GroupBox
+    Friend WithEvents StudentInformationGroupBox As GroupBox
+    Friend WithEvents MathProblemTypeGroupBox As GroupBox
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SummaryButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
